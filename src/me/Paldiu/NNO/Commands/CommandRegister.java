ba8ce61880687d45d303d007363cfaf66e3e9fba
Paul Reilly <paldiutheminecrafter@gmail.com>
2015-09-15 14:24:22 -0500

    Should fix issues.

--- src/me/Paldiu/NNO/Commands/CommandRegister.java
+++ src/me/Paldiu/NNO/Commands/CommandRegister.java
@@ -1,44 +1,112 @@
 package me.Paldiu.NNO.Commands;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.security.CodeSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import me.Paldiu.NNO.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
 
-public class CommandRegister {
+public class CommandRegister
+{
+    private static CommandMap cmap = getCommandMap();
+    private static final ArrayList<String> commands = new ArrayList<>();
 
-	public static void registerCommands() {
-		registerCommand(new Achgiv());
-		registerCommand(new AdminChat());
-		registerCommand(new AdminMode());
-		registerCommand(new Anal());
-		registerCommand(new Aoap());
-		registerCommand(new Bhrp());
-		registerCommand(new Cake());
-		registerCommand(new ChatBot());
-		registerCommand(new ChatClear());
-		registerCommand(new Cuil());
-		registerCommand(new Deafen());
-		registerCommand(new Debug());
-		registerCommand(new Devconf());
-		registerCommand(new Dragon());
-		registerCommand(new Emsd());
-		registerCommand(new Freeze());
-		registerCommand(new Fuckoff());
-		registerCommand(new Gcmd());
-		registerCommand(new Gold());
-		registerCommand(new Jelly());
-		registerCommand(new Lockdown());
-		registerCommand(new Lockup());
-		registerCommand(new Ltg());
-		registerCommand(new Meep());
-		registerCommand(new Nno());
-		registerCommand(new Nope());
-		registerCommand(new Ntoggle());
-		registerCommand(new Onsa());
-		registerCommand(new Packnenil());
-		registerCommand(new Penis());
-	}
+    public CommandRegister()
+    {
+        registerCommands();
+    }
+    
+    public static void unregisterCommands()
+    {
+        for(String name : commands)
+        {
+            Command cmd = cmap.getCommand(name);
+            cmd.unregister(cmap);
+        }
+    }
 
-	private static void registerCommand(Object commandHandler) {
-		CommandController.registerCommands(Main.getInstance(), commandHandler);
-	}
+    public static void registerCommands()
+    {
+        try
+        {
+            Pattern PATTERN = Pattern.compile("me/Paldiu/NNO/Commands/(Command_[^\\$]+)\\.class");
+            CodeSource codeSource = Main.class.getProtectionDomain().getCodeSource();
+            if (codeSource != null)
+            {
+                ZipInputStream zip = new ZipInputStream(codeSource.getLocation().openStream());
+                ZipEntry zipEntry;
+                while ((zipEntry = zip.getNextEntry()) != null)
+                {
+                    String entryName = zipEntry.getName();
+                    Matcher matcher = PATTERN.matcher(entryName);
+                    if (matcher.find())
+                    {
+                        try
+                        {
+                            Class<?> commandClass = Class.forName("me.Paldiu.NNO.Commands." + matcher.group(1));
+                            if (commandClass.isAnnotationPresent(CommandParameters.class))
+                            {
+                                Annotation annotation = commandClass.getAnnotation(CommandParameters.class);
+                                CommandParameters params = (CommandParameters) annotation;
+                                CommandController command = new BlankCommand(params.name(), params.usage(), params.description(), Arrays.asList(params.aliases().split(", ")), params.rank(), commandClass);
+                                command.register();
+                                commands.add(params.name());
+                            }
+                            else
+                            {
+                                Constructor construct = commandClass.getConstructor();
+                                CommandController command = (CommandController) construct.newInstance();
+                                command.register();
+                                commands.add(command.command);
+                            }
+                        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex)
+                        {
+                            Bukkit.broadcastMessage("" + ex);
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex)
+        {
+            Main.plugin.getLogger().severe(ex.getLocalizedMessage());
+        }
+    }
+
+    public static boolean isNNOCommand(String name)
+    {
+        return CommandRegistry.commands.contains(name.toLowerCase());
+    }
+
+    private static CommandMap getCommandMap()
+    {
+        if (cmap == null)
+        {
+            try
+            {
+                final Field f = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+                f.setAccessible(true);
+                cmap = (CommandMap) f.get(Bukkit.getServer());
+                return getCommandMap();
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else if (cmap != null)
+        {
+            return cmap;
+        }
+        return getCommandMap();
+    }
 
 }
