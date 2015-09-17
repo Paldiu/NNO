76a7956706c731973e0f403dc24b757c48633909
Paul Reilly <paldiutheminecrafter@gmail.com>
2015-09-16 20:43:45 -0500

    Create CommandParameters.java

---
+++ src/me/Paldiu/NNO/Commands/CommandParameters.java
@@ -0,0 +1,19 @@
+package me.Paldiu.NNO.Commands;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import me.Paldiu.NNO.Commands.CommandPermissions;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CommandParameters
+{
+    String name();
+
+    String description();
+
+    String usage();
+
+    String aliases() default "";
+
+    CommandPermissions permissions() default ;
+}
